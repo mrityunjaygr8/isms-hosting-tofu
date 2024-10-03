6d3526f1004232b5a097ed8708ccf8457af34d62
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-10-03 19:43:33 +0530

    http port is opened instead of https now

--- modules/isms/main.tf
+++ modules/isms/main.tf
@@ -112,13 +112,13 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "HTTPS"
+    name                       = "HTTP"
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
